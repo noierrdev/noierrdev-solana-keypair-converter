e9468ce147347fedfe43557552fbb26d010f51e0
vander moleker <vandermoleker@gmail.com>
2025-06-25 07:08:11 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,27 @@ use serde_json::from_str;
 async fn main() {
     let dir = "./"; // path to your directory
     for entry in fs::read_dir(dir) {
-        let entry = entry.unwrap();
-        let path = entry.path();
+        let entry = entry;
+        println!("{:?}", entry);
+        // let path = entry.path();
 
-        if path.extension().and_then(|s| s.to_str()) == Some("json") {
-            // Read file
-            let contents = fs::read_to_string(&path);
-            let secret_bytes: Vec<u8> = from_str(&contents).expect("valid JSON array");
-            let secret_arr: [u8; 64] = secret_bytes.try_into().expect("64-byte secret");
+        // if path.extension().and_then(|s| s.to_str()) == Some("json") {
+        //     // Read file
+        //     let contents = fs::read_to_string(&path);
+        //     let secret_bytes: Vec<u8> = from_str(&contents).expect("valid JSON array");
+        //     let secret_arr: [u8; 64] = secret_bytes.try_into().expect("64-byte secret");
 
-            // Encode as base58 string
-            let encoded = bs58::encode(secret_arr).into_string();
+        //     // Encode as base58 string
+        //     let encoded = bs58::encode(secret_arr).into_string();
 
-            // Save string representation to file
-            let out_path = path.with_extension("txt");
-            fs::write(&out_path, &encoded);
+        //     // Save string representation to file
+        //     let out_path = path.with_extension("txt");
+        //     fs::write(&out_path, &encoded);
 
-            // Remove the original .json file
-            // fs::remove_file(&path)?;
+        //     // Remove the original .json file
+        //     // fs::remove_file(&path)?;
 
-            println!("Converted and deleted {:?}", path);
+        //     println!("Converted and deleted {:?}", path);
         }
     }
 }
