9c6b96f9d6a0400445b59d5751640b03254a670a
vander moleker <vandermoleker@gmail.com>
2025-06-25 06:54:22 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
 
         if path.extension().and_then(|s| s.to_str()) == Some("json") {
             // Read file
-            let contents = fs::read_to_string(&path)
+            let contents = fs::read_to_string(&path);
             let secret_bytes: Vec<u8> = from_str(&contents).expect("valid JSON array");
             let secret_arr: [u8; 64] = secret_bytes.try_into().expect("64-byte secret");
 
@@ -20,7 +20,7 @@ async fn main() {
 
             // Save string representation to file
             let out_path = path.with_extension("txt");
-            fs::write(&out_path, &encoded)
+            fs::write(&out_path, &encoded);
 
             // Remove the original .json file
             // fs::remove_file(&path)?;
