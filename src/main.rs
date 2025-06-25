0a17123f08dcd867de013c81f6ea77857e1ff4a0
vander moleker <vandermoleker@gmail.com>
2025-06-25 06:50:55 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::path::Path;
 use serde_json::from_str;
 
-fn main() -> std::io::Result<()> {
+fn main()  {
     let dir = "./"; // path to your directory
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
