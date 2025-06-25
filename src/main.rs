3bea982f6043b9057306dda756486f96043606a8
vander moleker <vandermoleker@gmail.com>
2025-06-25 06:53:42 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use serde_json::from_str;
 #[tokio::main]
 async fn main() {
     let dir = "./"; // path to your directory
-    for entry in fs::read_dir(dir)? {
+    for entry in fs::read_dir(dir) {
         let entry = entry;
         let path = entry.path();
 
         if path.extension().and_then(|s| s.to_str()) == Some("json") {
             // Read file
-            let contents = fs::read_to_string(&path).await;
+            let contents = fs::read_to_string(&path)
             let secret_bytes: Vec<u8> = from_str(&contents).expect("valid JSON array");
             let secret_arr: [u8; 64] = secret_bytes.try_into().expect("64-byte secret");
 
@@ -20,7 +20,7 @@ async fn main() {
 
             // Save string representation to file
             let out_path = path.with_extension("txt");
-            fs::write(&out_path, &encoded).await;
+            fs::write(&out_path, &encoded)
 
             // Remove the original .json file
             // fs::remove_file(&path)?;
